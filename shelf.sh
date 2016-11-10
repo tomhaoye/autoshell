3e5fbb1f0ea33694484c98e0f2eb1b7dd622f854
邱奕玮 <tomhaoye@163.com>
2016-11-10 19:59:20 +0800

    file change will replace

--- shelf.sh
+++ shelf.sh
@@ -6,6 +6,7 @@ AR=/Users/qiuyiwei/dev/
 OPTFILE=/var/log/mytest/opt
 TIMEFILE=/var/log/mytest/timefile
 QUCHONG=/var/log/mytest/quchong
+REPLACE=/var/log/mytest/replace
 
 function for_in_file(){
 	filelist=$(ls $1)
@@ -14,7 +15,8 @@ function for_in_file(){
 			if [ -f $1/$file ];then
 				cat $1/$file | grep "^<script.*src" | while read LINE  
 					do
-						 echo $LINE | sed 's/^.*src="//g'| sed 's/".*$//g' | sed 's/^http.*$//g' | sed '/^$/d' | sed 's/\?.*$//g' >> $OPTFILE
+						 #echo $LINE | sed 's/^.*src="//g'| sed 's/".*$//g' | sed 's/^http.*$//g' | sed '/^$/d' | sed 's/\?.*$//g' >> $OPTFILE
+						 echo $LINE | sed 's/^.*src="//g'| sed 's/".*$//g' | sed 's/^http.*$//g' | sed '/^$/d'  >> $OPTFILE
 					done
 			fi
 			if [ -d $1/$file ];then
@@ -26,8 +28,9 @@ function for_in_file(){
 function add_file_time(){
 	cat $QUCHONG | while read LINE
 		do
+			LINE=$(echo $LINE | sed 's/\?.*$//g')
 			if [ -f $FILEROOT$LINE ];then
-				version=$(ls -full $FILEROOT$LINE | awk '{print $8}' | base64) 
+				version=$(ls -full $FILEROOT$LINE | awk '{print $5}' | base64) 
 				echo $LINE?v=$version >> $TIMEFILE
 			else
 				echo >> $TIMEFILE
@@ -41,7 +44,7 @@ function quchong(){
 }
 
 function for_in_script(){
-	cat $TIMEFILE | while read LINE
+	cat $REPLACE | while read LINE
 	do
 		PART=$(echo $LINE | sed '/^$/d' | sed 's/\?.*$//g' | sed "s:\/:\\\/:g")
 		LINE=$(echo $LINE | sed "s:\/:\\\/:g")
@@ -62,8 +65,32 @@ function replace_in_view(){
 	done
 }
 
+function diff_version(){
+	diff $QUCHONG $TIMEFILE -u | grep '^+/' | sed 's/+//g' > $REPLACE
+	rm $TIMEFILE
+}
+
+#function for_in_view(){
+#	filelist=$(ls $1)
+#	for file in $filelist
+#	do
+#		if [ -f $1/$file ];then
+#			cat $TIMELINE | while read LINE
+#			do
+#				PART=$(echo $LINE | sed '/^$/d' | sed 's/\?.*$//g' | sed "s:\/:\\\/:g")
+#				LINE=$(echo $LINE | sed "s:\/:\\\/:g")
+#				sed -i "" "s/src=\"$PART.*\"/src=\"$LINE\"/g" $1/$file
+#			done
+#		fi
+#		if [ -d $1/$file ];then
+#			for_in_view $1/$file
+#		fi
+#	done
+#}
 
 for_in_file $VIEW
 quchong
 add_file_time
+diff_version
 for_in_script
+#for_in_view $VIEW
